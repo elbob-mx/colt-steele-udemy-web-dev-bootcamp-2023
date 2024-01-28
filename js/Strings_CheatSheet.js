c550c05c4f033b07617b74af30cb44022856ef2c
elbob-mx <rob7th@gmail.com>
2024-01-28 12:43:12 -0600

    js update

--- js/Strings_CheatSheet.js
+++ js/Strings_CheatSheet.js
@@ -2,7 +2,7 @@
 let color = "purple";
 
 // Single quotes work too:
-let city = 'Tokyo';
+let city = "Tokyo";
 
 //Strings have a length:
 city.length; //5
@@ -12,17 +12,17 @@ city[0]; //'T'
 city[3]; //'y'
 
 // String methods:
-'hello'.toUpperCase(); // "HELLO";
-'LOL'.toLowerCase(); // "lol"
-'    omg  '.trim(); // "omg"
+"hello".toUpperCase(); // "HELLO";
+"LOL".toLowerCase(); // "lol"
+"    omg  ".trim(); // "omg"
 
 // String methods with arguments:
 // ==============================
 
 //indexOf returns the index where the character is found (or -1 if not found)
-'spider'.indexOf('i'); //2
-'vesuvius'.indexOf('u'); //3 - only returns FIRST matching index
-'cactus'.indexOf('z'); //-1 not found
+"spider".indexOf("i"); //2
+"vesuvius".indexOf("u"); //3 - only returns FIRST matching index
+"cactus".indexOf("z"); //-1 not found
 
 // slice - returns a "slice" of a string
 "pancake".slice(3); //"cake" - slice from index 3 onwards
@@ -35,7 +35,6 @@ city[3]; //'y'
 // Use backtick characters, NOT SINGLE QUOTES!
 // ========================
 const color = "olive green";
-const msg = `My favorite color is: ${color}` //"My favorite color is: olive green"
+const msg = `My favorite color is: ${color}`; //"My favorite color is: olive green"
 
-const str = `There are ${60 * 60 * 24} seconds in a day`//"There are 86400 seconds in a day"
-
+const str = `There are ${60 * 60 * 24} seconds in a day`; //"There are 86400 seconds in a day"
