a8ed9d695b95cfab5d553e7fd0f1533b7dfe399a
Roberto Gutiérrez <rob7th@gmail.com>
2024-02-19 23:33:00 -0600

    sas

--- js/app/app-2.js
+++ js/app/app-2.js
@@ -1,25 +1,47 @@
-console.log("conditionals!");
+// console.log("conditionals!");
 
-if (1 == true) {
-  console.log("yes, 1 is indeed true");
-}
+// if (1 == true) {
+//   console.log("yes, 1 is indeed true");
+// }
 
-if (1 + 3 === 4) {
-  console.log("sure, 1 + 3 it's 4");
-}
+// if (1 + 3 === 4) {
+//   console.log("sure, 1 + 3 it's 4");
+// }
 
-console.log("Random number:");
-console.log(Math.random());
+// // RANDOM NUM
+// console.log("Random number:");
+// console.log(Math.random());
 
-let randomNum = Math.round(Math.random()) * 3;
-console.log(randomNum);
+// let randomNum = Math.round(Math.random()) * 3;
+// console.log(randomNum);
 
-if (randomNum === 3) {
-  console.log(randomNum);
-  console.log("it is!");
-}
+// if (randomNum === 3) {
+//   console.log(randomNum);
+//   console.log("it is!");
+// }
 
-if (randomNum !== 3) {
-  console.log(randomNum);
-  console.log("it's not");
+// if (randomNum !== 3) {
+//   console.log(randomNum);
+//   console.log("it's not");
+// }
+
+// // ODD NUM
+// let num = 10;
+// console.log(num);
+
+// function isEven(num) {
+//   if (num % 2 == 0) {
+//     console.log("even");
+//   } else if (num % 3 == 0) {
+//     console.log("odd!");
+//   }
+// }
+
+const dayOfTheWeek = "Sunday";
+if (dayOfTheWeek === "Monday") {
+  console.log("monday, not again...");
+} else if (dayOfTheWeek === "Saturday") {
+  console.log("yeah! saturday!");
+} else if (dayOfTheWeek === "Sunday") {
+  console.log("well... weekend is over...");
 }
