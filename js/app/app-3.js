ed31c667dcd85e8a767f55da15344ab4dff57eca
elbob-mx <rob7th@gmail.com>
2024-02-21 17:03:58 -0600

    update js 3

---
+++ js/app/app-3.js
@@ -0,0 +1,14 @@
+const password = prompt(
+  "please enter a new password (must be 6+ chars please)"
+);
+
+// at least 6 chars password
+if (password.length >= 6) {
+  if (password.indexOf(" ") === -1) {
+    alert("valid password");
+  } else {
+    alert("invalid password, try again");
+  }
+} else alert("invalid password, try again");
+
+// no space no password
