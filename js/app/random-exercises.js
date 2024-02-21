7a5b1f5109b45e319d365a005c6ebf526288aea6
elbob-mx <rob7th@gmail.com>
2024-02-21 17:48:44 -0600

    update

--- js/app/random-exercises.js
+++ js/app/random-exercises.js
@@ -1,24 +1,30 @@
 // Change the value of num, so that "YOU GOT ME!" prints out
-const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :)
+// const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :)
 
-// DO NOT TOUCH ANYTHING BELOW (please)
-if (num <= 100) {
-  if (num >= 50) {
-    console.log("HEY!");
-  }
-} else {
-  if (num < 103) {
-    if (num % 2 === 0) {
-      console.log("YOU GOT ME!");
-    }
-  }
-}
+// // DO NOT TOUCH ANYTHING BELOW (please)
+// if (num <= 100) {
+//   if (num >= 50) {
+//     console.log("HEY!");
+//   }
+// } else {
+//   if (num < 103) {
+//     if (num % 2 === 0) {
+//       console.log("YOU GOT ME!");
+//     }
+//   }
+// }
 
 // TRUTHY FALSY
-const userInput = prompt("Enter random text");
+// const userInput = prompt("Enter random text");
 
-if (userInput) {
-  alert("well done, genius. That's a TRUTHY");
+// if (userInput) {
+//   alert("well done, genius. That's a TRUTHY");
+// } else {
+//   alert("that doesn't work. That's a FALSY");
+// }
+
+if ("undefined") {
+  console.log("truthy!");
 } else {
-  alert("that doesn't work. That's a FALSY");
+  console.log("falsey!");
 }
