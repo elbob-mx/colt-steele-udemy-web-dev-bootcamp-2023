c6b57e655629393cbebf6093feb53f7ba7554db4
elbob-mx <rob7th@gmail.com>
2024-02-22 00:32:34 -0600

    js update

--- js/app/random-exercises.js
+++ js/app/random-exercises.js
@@ -1,50 +1,57 @@
 // Change the value of num, so that "YOU GOT ME!" prints out
-// const num = 102; // THIS IS THE ONLY LINE YOU SHOULD CHANGE :)
+const num = 102;
 
-// // DO NOT TOUCH ANYTHING BELOW (please)
-// if (num <= 100) {
-//   if (num >= 50) {
-//     console.log("HEY!");
-//   }
-// } else {
-//   if (num < 103) {
-//     if (num % 2 === 0) {
-//       console.log("YOU GOT ME!");
-//     }
-//   }
-// }
+if (num <= 100) {
+  if (num >= 50) {
+    console.log("HEY!");
+  }
+} else {
+  if (num < 103) {
+    if (num % 2 === 0) {
+      console.log("YOU GOT ME!");
+    }
+  }
+}
 
-// TRUTHY FALSY
-// const userInput = prompt("Enter random text");
+// TRUTHY & FALSY
+const userInput = prompt("Enter random text");
 
-// if (userInput) {
-//   alert("well done, genius. That's a TRUTHY");
-// } else {
-//   alert("that doesn't work. That's a FALSY");
-// }
+if (userInput) {
+  alert("well done, genius. That's a TRUTHY");
+} else {
+  alert("that doesn't work. That's a FALSY");
+}
 
-// if ("undefined") {
-//   console.log("truthy!");
-// } else {
-//   console.log("falsey!");
-// }
+if ("undefined") {
+  console.log("truthy!");
+} else {
+  console.log("falsey!");
+}
 
-// AND
+// AND OPERATOR
+if (1 <= 4 && "abc".length === 3) {
+  console.log("false, but true indeed");
+}
 
-// if (1 <= 4 && "abc".length === 3) {
-//   console.log("false, but true indeed");
-// }
+const password = "1234 567";
+if (password.length >= 6 && password.indexOf(" ") === -1) {
+  console.log("Valid password.");
+} else {
+  console.log("Invalid password");
+}
 
-// const password = "1234 567";
-// if (password.length >= 6 && password.indexOf(" ") === -1) {
-//   console.log("Valid password.");
-// } else {
-//   console.log("Invalid password");
-// }
-
-const mystery = "PiValue7"; //CHANGE THIS VALUE TO MAKE THE CONDITIONAL BELOW TRUE
-
-// LEAVE THIS CODE ALONE! (pretty please)
+// EXERCISE
+const mystery = "PiValue7";
 if (mystery[0] === "P" && mystery.length > 5 && mystery.indexOf("7") !== -1) {
   console.log("YOU GOT IT!!!");
+}
+
+// OR OPERATOR
+const age = 8;
+if (age <= 5 || age >= 65) {
+  console.log("Free!");
+} else if (age <= 10) {
+  console.log("$5");
+} else if (age < 65) {
+  console.log("$20");
 }
