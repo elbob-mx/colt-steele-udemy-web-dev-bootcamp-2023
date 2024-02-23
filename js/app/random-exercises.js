07603c1316e200d1cda3c1f52b7a9664355cb723
elbob-mx <rob7th@gmail.com>
2024-02-22 18:39:29 -0600

    js random ex update

--- js/app/random-exercises.js
+++ js/app/random-exercises.js
@@ -66,12 +66,57 @@
 // } else console.log("wow!");
 
 // not operator
-const age = 65;
+// const age = 65;
 
-if (age === 0 || age <= 3) {
-  console.log("Error. Try again");
-} else if (!((age >= 4 && age < 10) || age >= 65)) {
-  alert("You'll pay $15.00");
-} else {
-  console.log("Free of charge.");
+// if (age === 0 || age <= 3) {
+//   console.log("Error. Try again");
+// } else if (!((age >= 4 && age < 10) || age >= 65)) {
+//   alert("You'll pay $15.00");
+// } else {
+//   console.log("Free of charge.");
+// }
+
+// SWITCH (not that common)
+// const day = 5;
+// if (day === 1) {
+//   console.log("monday");
+// } else if (day === 2) {
+//   console.log("tuesday");
+// } else if (day === 3) {
+//   console.log("wednesday");
+// } else if (day === 4) {
+//   console.log("thursday");
+// } else if (day === 5) {
+//   console.log("friday");
+// } else if (day === 6) {
+//   console.log("saturday");
+// } else if (day === 7) {
+//   console.log("sunday");
+// } else {
+//   console.log("error.");
+// }
+
+const day = 6;
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+    console.log("wednesday");
+    break;
+  case 4:
+    console.log("thursday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  case 6:
+  case 7:
+    console.log("weekend!");
+    break;
+  default:
+    console.log("error.");
 }
