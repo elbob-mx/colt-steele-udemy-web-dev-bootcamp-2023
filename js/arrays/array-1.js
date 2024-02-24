73d0be2286d3356998aaecd4667046d452570166
Roberto Gutiérrez <rob7th@gmail.com>
2024-02-24 12:14:13 -0600

    array 101 update

--- js/arrays/array-1.js
+++ js/arrays/array-1.js
@@ -1,17 +1,24 @@
 let pc = ["mobo", "cpu", "gpu", "monitor", "keeb", "mouse"];
-console.log(pc);
 
 pc.push("wifi", "printer", "router");
-console.log(pc);
 
-pc.splice(1, 2 | 1, "intel");
-console.log(pc);
+pc.splice(1, 2 | 1, "intel 12300F");
 
 pc.splice(2, 3 | 2, "rtx 3060");
+
+pc.splice(0, 1 | 0, "asus TUF wifi mobo");
+
+pc.push("wifi", "printer", "mouse");
+
+pc.splice(7, 8 | 6, "basilisk x");
+
+pc.push("logitech mx keys");
+
+let specs = pc.length;
 console.log(pc);
 
-pc.splice(0, 1 | 0, "asus TUF wifi");
-console.log(pc);
-
-pc.push("wifi", "printer");
-console.log(pc);
+if (pc.length === 5) {
+  console.log(pc.length + "pieces nice setup");
+} else {
+  console.log(pc.length + " pieces, more than enough eqipment, but maybe...");
+}
