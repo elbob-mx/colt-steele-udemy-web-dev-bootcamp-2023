3ff48417903c0642501741b5eb55e90ba29300ac
Roberto Gutiérrez <rob7th@gmail.com>
2024-03-15 15:37:23 -0600

    update

--- js/arrays/splice-slice.js
+++ js/arrays/splice-slice.js
@@ -13,18 +13,23 @@ for (var i = 0; i < bands.length; i++) {
 
 // VARIABLES
 let industrialBands = bands.slice(0, 1);
-let proggyBands = bands.slice(3, 6); // starts at index 3 and includes it; ends at index 6 but doesnt include it
-let lastBands = bands.slice(6, 8);
+let proggyBands = bands.slice(2, 5); // starts at index 3 and includes it; ends at index 6 but doesnt include it
+let lastBands = bands.slice(5, 7);
 
 // CONSOLE LOGS
 console.log("Tour 2024:" + bands + ": confirmed bands so far.");
-console.log("presenting " + bands.length + " bands");
+console.log("presenting " + bands.length + " bands.");
 console.log(industrialBands + " is an idustrial band.");
 console.log(proggyBands + " are proggy bands.");
 console.log(lastBands + " are the last bands to present.");
 
 let orderedBands = bands.sort();
-console.log(orderedBands); // alphabetized bands
+console.log(orderedBands.toString() + " (bands alphabetized)."); // array turn to strings and alphabetized
 
 let prices = [100, 180, 250, 50, 500, 850, 1000, 85];
-console.log(prices.sort((a, b) => a - b)); // sorts comparing numeric value (85, 100, 180, 250, 500, 850, 1000)
+
+// add space at beggining of every string on 'bands' array
+for (var i = 0; i < prices.length; i++) {
+  prices[i] = " " + prices[i];
+}
+console.log("available seat prices:" + prices.sort((a, b) => a - b) + " (USD)"); // sorts comparing numeric value (85, 100, 180, 250, 500, 850, 1000)
