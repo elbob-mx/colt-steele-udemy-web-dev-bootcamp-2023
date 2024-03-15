ce88af5ffbc1aaa88ef2df0f8131a6d1f1a8dd6d
Roberto Gutiérrez <rob7th@gmail.com>
2024-03-15 14:46:29 -0600

    splice slice update

--- js/arrays/splice-slice.js
+++ js/arrays/splice-slice.js
@@ -1,5 +1,7 @@
 // VARIABLES
-let bands = ["NIN", "deftones", "pantera", "meshuggah", "isis", "tool"];
+let bands = ["NIN", "Deftones", "Pantera", "Meshuggah", "Isis", "TOOL"];
+// SPLICE
+let joiningBand = bands.splice(6, 0, "Red Fang", "Chevelle");
 
 // FUNCTIONS
 // add space at beggining of every string on 'bands' array
@@ -10,8 +12,11 @@ for (var i = 0; i < bands.length; i++) {
 // VARIABLES
 let industrialBands = bands.slice(0, 1);
 let proggyBands = bands.slice(3, 6); // starts at index 3 and includes it; ends at index 6 but doesnt include it
+let lastBands = bands.slice(6, 8); // starts at
 
 // CONSOLE LOGS
-console.log(bands);
+console.log("Tour 2024:" + bands + ": confirmed bands so far.");
+console.log("presenting " + bands.length + " bands");
 console.log(industrialBands + " is an idustrial band.");
 console.log(proggyBands + " are proggy bands.");
+console.log(lastBands + " are the last bands to present.");
