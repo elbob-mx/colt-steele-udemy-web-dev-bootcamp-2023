833bbd11d1652f4d7d42c8030acff55a2d3009ba
elbob-mx <rob7th@gmail.com>
2024-03-24 14:04:02 -0600

    update objects

--- js/objects/object.js
+++ js/objects/object.js
@@ -1,4 +1,4 @@
-console.log(typeof {});
+/* console.log(typeof {});
 console.log(typeof []);
 
 // object literal example
@@ -35,7 +35,7 @@ console.log(
   `Ok😁!, for ${product.name} we have these flavors: ${product.flavors.join(
     ", "
   )}. Their price is $${product.price} each.`
-);
+);*/
 
 /* object to array example (credits to: https://stackoverflow.com/users/13555735/akshay-pagare)
 let obj = { x: 1, y: 2, z: 3 };
@@ -46,15 +46,24 @@ for (let i = 0; i < Object.keys(obj).length; i++) {
 
 console.log(outputArray); */
 
-const midterms = { danielle: 96, thomas: 78 };
-console.log(midterms);
-midterms.thomas = 79; //fix grade typo
-console.log(midterms);
-midterms.thomas = "C+"; // change to letter grades
-console.log(midterms);
-midterms.danielle = "A";
-console.log(midterms);
-midterms.ezra = "B+"; // adding new person with grade
-console.log(midterms);
-midterms["antonio"] = "A-"; // adding new person's grade using brackets
-console.log(midterms);
+// const midterms = { danielle: 96, thomas: 78 };
+// console.log(midterms);
+// midterms.thomas = 79; //fix grade typo
+// console.log(midterms);
+// midterms.thomas = "C+"; // change to letter grades
+// console.log(midterms);
+// midterms.danielle = "A";
+// console.log(midterms);
+// midterms.ezra = "B+"; // adding new person with grade
+// console.log(midterms);
+// midterms["antonio"] = "A-"; // adding new person's grade using brackets
+// console.log(midterms);
+
+// nesting arrays and objects
+const comments = [
+  { username: "Mary", text: "lol! XD", votes: 9 },
+  { username: "elPez", text: "salmon rules", votes: 327 },
+];
+
+console.log(comments);
+console.log(comments[1].text);
