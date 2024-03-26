c1c2cbde60920b21bdbb8718e96ba4db29365f7d
Roberto Gutiérrez <rob7th@gmail.com>
2024-03-26 13:09:32 -0600

    update every nav bar

--- js/loops/guess-game.js
+++ js/loops/guess-game.js
@@ -1,3 +1,34 @@
+// let maxNum = parseInt(
+//   prompt("Let's make a numbers range. Tell us the maximum number:")
+// );
+// while (!maxNum) {
+//   maxNum = parseInt(prompt("Please enter a valid number:"));
+// }
+
+// const targetNum = Math.floor(Math.random() * maxNum + 1);
+// console.log(targetNum);
+
+// let attempt = parseInt(prompt("Come on, try to guess the number:"));
+// let guesses = 1;
+
+// while (parseInt(attempt) !== targetNum) {
+//   if (attempt === "quit") break;
+//   guesses++;
+//   if (attempt > targetNum) {
+//     attempt = prompt("nope. too high. try again:");
+//   } else {
+//     attempt = prompt("nope. too low. try again:");
+//   }
+// }
+// if (attempt === "quit") {
+//   prompt("maybe too hard for you.");
+// } else {
+//   prompt(
+//     `alright, alright, alright. Took you ${guesses} guesses. I guess you should be proud ¬¬`
+//   );
+// }
+
+// fixed:
 let maxNum = parseInt(
   prompt("Let's make a numbers range. Tell us the maximum number:")
 );
@@ -8,22 +39,24 @@ while (!maxNum) {
 const targetNum = Math.floor(Math.random() * maxNum + 1);
 console.log(targetNum);
 
-let attempt = parseInt(prompt("Come on, try to guess the number:"));
+let attempt = prompt("Come on, try to guess the number:");
 let guesses = 1;
 
 while (parseInt(attempt) !== targetNum) {
   if (attempt === "quit") break;
-  guesses++;
+  attempt = parseInt(attempt);
   if (attempt > targetNum) {
     attempt = prompt("nope. too high. try again:");
-  } else {
+    guesses++;
+  } else if (attempt < targetNum) {
     attempt = prompt("nope. too low. try again:");
+    guesses++;
+  } else {
+    attempt = prompt("Not valid. Enter a number or 'quit' to end game");
   }
 }
 if (attempt === "quit") {
-  prompt("maybe too hard for you.");
+  prompt(`Game over. the number was: ${targetNum}. Better luck next time.`);
 } else {
-  prompt(
-    `alright, alright, alright. Took you ${guesses} guesses. I guess you should be proud ¬¬`
-  );
+  prompt(`alright, alright, alright. Took you ${guesses} guesses.`);
 }
