fd5815c15905863ad208a8343caa504a8a481643
Roberto Gutierrez <rob7th@gmail.com>
2024-04-01 16:06:16 -0600

    update

--- js/functions/simple-functions.js
+++ js/functions/simple-functions.js
@@ -8,9 +8,30 @@
 rant("go!"); */
 
 // two arguments in function
-function principalSkinner(firstName, lastName) {
-  console.log(`Lets say; ${firstName} ${lastName[0]}.`);
-  console.log(`No wait, I meant, ${firstName[0]}. ${lastName}.`);
+/* function principalSkinner(firstName, lastName) {
+  console.log(`Lets say; "${firstName} ${lastName[0]}".`); // getting the initial using index
+  console.log(`No, no, no, lets say, "${firstName[0]}. ${lastName}".`); // getting the initial using index
+}
+principalSkinner("Lisa", "Simpson"); */
+
+// function with prompts asking for arguments
+/* function repeatText(text, reps) {
+  let words = prompt("Write a word:");
+  text = words;
+  let num = prompt("Write a number");
+  reps = num;
+  for (i = 2; i <= reps; i++) {
+    text += words + ", ";
+  }
+  console.log(text);
+} */
+
+function repeat(text, reps) {
+  let result = "";
+  for (let i = 1; i <= reps; i++) {
+    result += text + "... ";
+  }
+  console.log(result);
 }
 
-principalSkinner("Lisa", "Simpson");
+repeat("alright", 3);
