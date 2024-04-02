ab2cae87d2cda4ab74b90d8def77e9446157a2bf
Roberto Gutierrez <rob7th@gmail.com>
2024-04-02 14:35:09 -0600

    sum array examples

---
+++ js/functions/sum-array.js
@@ -0,0 +1,30 @@
+let array = [1, 2, 3, 5]; // 11
+
+//for loop
+/* function sumArray(array) {
+  let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
+}
+console.log(sumArray(array)); */
+
+//forEach loop
+/* function sumArray(array) {
+  let total = 0;
+  array.forEach((e) => {
+    total += e;
+  });
+  return total;
+}
+
+console.log(sumArray(array)); */
+
+// reduce() loop
+function sumArray(array) {
+  let total = array.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+}
+console.log(sumArray(array));
