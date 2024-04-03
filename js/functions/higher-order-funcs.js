f6a17b6c4b0c72ebb5143973534d3acbf7e5648a
Roberto Gutierrez <rob7th@gmail.com>
2024-04-03 12:38:16 -0600

    methods update

--- js/functions/higher-order-funcs.js
+++ js/functions/higher-order-funcs.js
@@ -1,18 +1,33 @@
-function rollTwoDices(funk) {
-  funk();
-  funk();
+// function rollTwoDices(funk) {
+//   funk();
+//   funk();
+// }
+
+// function pullTenNumbers(f) {
+//   for (let i = 0; i <= 9; i++) {
+//     f();
+//   }
+// }
+
+// function rollDice() {
+//   const roll = Math.floor(Math.random() * 6) + 1;
+//   console.log(roll);
+// }
+
+// console.log(rollTwoDices(rollDice));
+// console.log(pullTenNumbers(rollDice));
+
+function makeRangeFunc(min, max) {
+  return function (num) {
+    return num >= min && num <= max;
+  };
 }
 
-function pullTenNumbers(f) {
-  for (let i = 0; i <= 9; i++) {
-    f();
-  }
-}
+const isMinor = makeRangeFunc(0, 18);
+console.log(isMinor(12)); // true
 
-function rollDice() {
-  const roll = Math.floor(Math.random() * 6) + 1;
-  console.log(roll);
-}
+const isAdult = makeRangeFunc(19, 64);
+console.log(isAdult(53)); // true
 
-console.log(rollTwoDices(rollDice));
-console.log(pullTenNumbers(rollDice));
+const isSenior = makeRangeFunc(65, 110);
+console.log(isSenior(72)); // true
