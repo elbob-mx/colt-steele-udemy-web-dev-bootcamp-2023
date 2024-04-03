96f616b08ef011b22d6c4a83903142c889ef23d8
Roberto Gutierrez <rob7th@gmail.com>
2024-04-02 18:07:59 -0600

    update

--- js/functions/func-expressions.js
+++ js/functions/func-expressions.js
@@ -1,4 +1,11 @@
-const square = function (num) {
+// save (and use) function as a variable
+/* const square = function (num) {
   return num * num;
 };
-console.log(square(10)); // 100
+console.log(square(10)); // 100 */
+
+// save (and use) function as a variable
+const square = function (num) {
+  return Math.pow(num, 2);
+};
+console.log(square(10));
