7ff0f31ef42b6a29399f539e4c0030e451f6f537
Roberto Gutierrez <rob7th@gmail.com>
2024-04-09 16:28:13 -0600

    every

--- js/array callback methods/every.js
+++ js/array callback methods/every.js
@@ -1,11 +1,16 @@
-const words = ["dog", "dig", "log", "bag", "wag"];
+// const examScores = [75, 80, 90, 60, 83, 98];
 
-words.every((w) => {
-  let last_letter = w[w.length - 1];
-  console.log(last_letter === "g");
-}); // true
+// let classroomPassed = examScores.every((score) => score >= 70);
+// console.log(classroomPassed); // false
+
+const words = ["dog", "dig", "log", "bag", "wag"];
 
 // words.every((w) => {
 //   let last_letter = w[w.length - 1];
 //   return last_letter === "g";
 // }); // true
+
+words.every((w) => {
+  let last_letter = w[w.length - 1];
+  console.log(last_letter === "g");
+}); // true
