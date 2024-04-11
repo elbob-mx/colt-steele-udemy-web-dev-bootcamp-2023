0a9e6259419e26a2241ed1d64789c91d5a47d7d6
elbob-mx <rob7th@gmail.com>
2024-04-11 00:07:21 -0600

    default params new way js

--- js/newer js functions/default-params.js
+++ js/newer js functions/default-params.js
@@ -1,8 +1,26 @@
+// old way:
 function rollDice(numSides) {
   if (numSides === undefined) {
     numSides = 6;
   }
   return Math.floor(Math.random() * numSides) + 1;
 }
-
 console.log(rollDice()); // if not value is passed, value will be '6' by default
+
+//new way:
+function rollNewDice(sides = 6) {
+  return Math.floor(Math.random() * sides) + 1;
+}
+console.log(rollNewDice(20)); // if not value is passed, value will be '6' by default
+
+// new way:
+function multiply(a, b = 1) {
+  return a * b;
+}
+console.log(multiply(4, 5)); // 20
+
+// new way:
+function greet(personName, msg = "hello", punctuation = "!") {
+  return `${msg}, ${personName}${punctuation}`;
+}
+console.log(greet("bob"));
