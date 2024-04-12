746703f1dca4e490b291cfed0d59862c8b53f06a
Roberto Gutierrez <rob7th@gmail.com>
2024-04-12 17:11:28 -0600

    destructuring

--- js/newer js functions/destructuring.js
+++ js/newer js functions/destructuring.js
@@ -1,19 +1,32 @@
 // a short, clean syntax to 'UNPACK' values form arrays, properties from objects into distinct variables.
 
-const raceResults = ["Laura Dennis", "Jay Riley", "Evan Miller"];
+// const raceResults = ["Laura Dennis", "Jay Riley", "Evan Miller"];
 
-const [gold, silver, bronze] = raceResults;
-gold;
-silver;
-bronze;
+// const [gold, silver, bronze] = raceResults;
+// gold;
+// silver;
+// bronze;
 
-const [fastest, ...everyoneElse] = raceResults;
-fastest;
-everyoneElse;
+// const [fastest, ...everyoneElse] = raceResults;
+// fastest;
+// everyoneElse;
 
 // console.log(raceResults);
-console.log("Gold goes to: " + gold);
-console.log("Silver goes to: " + silver);
-console.log("Bronze goes to: " + bronze);
-console.log("Speed record broken by: " + fastest);
-console.log("Thanks to the participants: " + everyoneElse.join(", "));
+// console.log("Gold goes to: " + gold);
+// console.log("Silver goes to: " + silver);
+// console.log("Bronze goes to: " + bronze);
+// console.log("Speed record broken by: " + fastest);
+// console.log("Thanks to the participants: " + everyoneElse.join(", "));
+
+/* ----------------------- */
+/* ----------------------- */
+
+const scores = [153489, 713468, 165876, 456679, 348671, 248679, 835798];
+
+// const hiScore = scores[0];
+// const secondHiScore = scores[1];
+
+const [gold, silver, bronze] = scores;
+console.log(gold); // first number in 'scores' array
+console.log(silver); // second number in 'scores' array
+console.log(bronze); // third number in 'scores' array
