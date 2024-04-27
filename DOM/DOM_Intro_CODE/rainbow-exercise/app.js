a0b74f004f1c07a3d0b15050cf5fe87df9050673
Roberto Gutierrez <rob7th@gmail.com>
2024-04-27 12:47:25 -0600

    update DOM

---
+++ DOM/DOM_Intro_CODE/rainbow-exercise/app.js
@@ -0,0 +1,8 @@
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOUR CODE GOES HERE:
+const rainbowLetters = document.getElementsByTagName("span");
+
+for (let i = 0; i < colors.length; i++) {
+  rainbowLetters[i].style.color = colors[i];
+}
