25804789f0a8f825be01806c16ad0bc9f2d20f6e
Roberto Gutierrez <rob7th@gmail.com>
2024-05-04 10:59:13 -0600

    update

--- js/app/movie-score-order.js
+++ js/app/movie-score-order.js
@@ -3,48 +3,48 @@ console.log("*** / CINEMA RATINGS / ***");
 console.log("");
 
 const movies = [
-  {
-    title: "Good Will Hunting",
-    score: 100,
-    year: 1997,
-    genre: "drama",
-  },
-  {
-    title: "Interstellar",
-    score: 99,
-    year: 2014,
-    genre: "sci-fi",
-  },
-  {
-    title: "The Shining",
-    score: 97,
-    year: 1980,
-    genre: "horror",
-  },
-  {
-    title: "The Godfather",
-    score: 98,
-    year: 1972,
-    genre: "thriller",
-  },
-  {
-    title: "Before Sunrise",
-    score: 99,
-    year: 1995,
-    genre: "drama",
-  },
-  {
-    title: "Tombstone",
-    score: 88,
-    year: 1993,
-    genre: "western",
-  },
-  {
-    title: "White Men Can't Jump",
-    score: 89,
-    year: 1992,
-    genre: "drama",
-  },
+	{
+		title: "Good Will Hunting",
+		score: 100,
+		year: 1997,
+		genre: "drama",
+	},
+	{
+		title: "Interstellar",
+		score: 99,
+		year: 2014,
+		genre: "sci-fi",
+	},
+	{
+		title: "The Shining",
+		score: 97,
+		year: 1980,
+		genre: "horror",
+	},
+	{
+		title: "The Godfather",
+		score: 98,
+		year: 1972,
+		genre: "thriller",
+	},
+	{
+		title: "Before Sunrise",
+		score: 99,
+		year: 1995,
+		genre: "drama",
+	},
+	{
+		title: "Tombstone",
+		score: 88,
+		year: 1993,
+		genre: "western",
+	},
+	{
+		title: "White Men Can't Jump",
+		score: 89,
+		year: 1992,
+		genre: "drama",
+	},
 ];
 
 console.log("According to the latest reviews: ");
@@ -52,12 +52,12 @@ console.log("");
 
 // descendant order
 function rateDown() {
-  console.log("By Score | Descending order ↓ :");
-  let descend = movies.sort((a, b) => b.score - a.score);
-  // console.log(`- ${movies[0].title} | ${movies[0].score} / 100`);
-  for (i = 0; i < descend.length; i++) {
-    console.log(`- ${descend[i].title} | ${descend[i].score} / 100`);
-  }
+	console.log("By Score | Descending order ↓ :");
+	let descend = movies.sort((a, b) => b.score - a.score);
+	// console.log(`- ${movies[0].title} | ${movies[0].score} / 100`);
+	for (i = 0; i < descend.length; i++) {
+		console.log(`- ${descend[i].title} | ${descend[i].score} / 100`);
+	}
 }
 rateDown();
 
@@ -67,12 +67,12 @@ console.log("");
 
 // ascendant order
 function rateUp() {
-  console.log("By Score | Ascending order ↑ :");
-  let ascend = movies.sort((a, b) => a.score - b.score);
-  // console.log(`- ${movies[0].title} | ${movies[0].score} / 100`);
-  for (i = 0; i < ascend.length; i++) {
-    console.log(`- ${ascend[i].title} | ${ascend[i].score} / 100`);
-  }
+	console.log("By Score | Ascending order ↑ :");
+	let ascend = movies.sort((a, b) => a.score - b.score);
+	// console.log(`- ${movies[0].title} | ${movies[0].score} / 100`);
+	for (i = 0; i < ascend.length; i++) {
+		console.log(`- ${ascend[i].title} | ${ascend[i].score} / 100`);
+	}
 }
 rateUp();
 
