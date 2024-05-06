0f25ca5f079c997bfab483c054639dae2269a879
Roberto Gutierrez <rob7th@gmail.com>
2024-05-05 23:54:44 -0600

    update

---
+++ DOM/btns.js
@@ -0,0 +1,9 @@
+const cont = document.querySelector("#container"); // we declare the container div
+
+// we create a loop to render 100 buttons; a button each loop
+for (let i = 0; i < 100; i++) {
+    const btn = document.createElement("button"); // we declare and create a <button> element
+    const div = document.querySelector("div"); // we declare and call the <div> with the "container" ID
+    btn.innerText = "copy of a"; // we put a text on the button
+    div.appendChild(btn); // we append the button child on the div parent
+}
