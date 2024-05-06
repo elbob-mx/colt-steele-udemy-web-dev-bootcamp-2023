54b7b1757e24c1b523b199a5cf68efa4b717085e
Roberto Gutierrez <rob7th@gmail.com>
2024-05-06 13:23:57 -0600

    update 1000 btns

--- DOM/btns.js
+++ DOM/btns.js
@@ -1,7 +1,7 @@
 const cont = document.querySelector("#container"); // we declare the container div
 
 // we create a loop to render 100 buttons; a button each loop
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 1000; i++) {
     const btn = document.createElement("button"); // we declare and create a <button> element
     const div = document.querySelector("div"); // we declare and call the <div> with the "container" ID
     btn.innerText = "copy of a"; // we put a text on the button
