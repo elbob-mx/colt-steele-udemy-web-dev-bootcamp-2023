c584087539c6850ec16e50a429e137dec2626721
Roberto Gutierrez <rob7th@gmail.com>
2024-05-07 00:16:06 -0600

    can-drink update

--- js/app/can-drink.js
+++ js/app/can-drink.js
@@ -1,19 +1,31 @@
-function canDrink(person) {
-    if (person?.age != null) {
-        if (person.age < 18) {
-            console.log("nope 🤮");
-        } else if (person.age < 21) {
-            console.log("not in the US 😞");
-        } else {
-            console.log("to the pub 🍻!");
-        }
-    } else {
-        console.log("waiting for an answer...");
-    }
+//  function canDrink(person) {if (person?.age != null) {
+//      if (person.age < 18) {
+//          console.log("nope 🤮");
+//      } else if (person.age < 21) {
+//          console.log("not in the US 😞");
+//      } else {
+//          console.log("to the pub 🍻!");
+//      }
+//  } else {
+//          console.log("waiting for an answer...");
+//      }
+//  }
+
+// function ableToDrink(person) {
+//     if (person?.age == null) return console.log("you're not human 🤨");
+//     else if (person.age < 18) return console.log("nope 🤮");
+//     else if (person.age < 21) return console.log("not in the US 😞");
+//     else return console.log("to the pub 🍻!");
+// }
+
+function canDrinkResponse(age) {
+    if (age < 18) return console.log("Nope");
+    if (age < 21) return console.log("Not in the US.");
+    return console.log("Yes 🍻");
 }
 
 const p = {
     age: 20,
 };
 
-canDrink(p);
+canDrinkResponse(p.age);
