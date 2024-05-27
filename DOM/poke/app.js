0a05f889a67d36d78708397c9a36af53314b8bbc
Roberto Gutierrez <rob7th@gmail.com>
2024-05-27 15:33:46 -0600

    update

---
+++ DOM/poke/app.js
@@ -0,0 +1,17 @@
+// VARIABLES
+const container = document.querySelector("#container");
+const baseUrl = "https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/";
+
+// CREATE IMAGES
+for (let i = 1; i <= 151; i++) {
+    const pokemon = document.createElement("div");
+    pokemon.classList.add("pokemon");
+    const label = document.createElement("span");
+    label.innerText = `# ${i}`;
+    const newImg = document.createElement("img");
+    newImg.src = `${baseUrl}${i}.png`;
+    // CREATE IMAGE WITH APPENDED NUMBER
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
+}
