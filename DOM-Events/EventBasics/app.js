521ad89e9cfc330debf0cd133cbd390ee5e8c0ff
Roberto Gutierrez <rob7th@gmail.com>
2024-06-05 17:08:39 -0600

    update

--- DOM-Events/EventBasics/app.js
+++ DOM-Events/EventBasics/app.js
@@ -1,39 +1,40 @@
-const btn = document.querySelector('#v2');
+const btn = document.querySelector("#v2");
 
 btn.onclick = function () {
-    console.log("YOU CLICKED ME!")
-    console.log("I HOPE IT WORKED!!")
-}
+    console.log("YOU CLICKED ME!");
+    console.log("I HOPE IT WORKED!!");
+};
 
 function scream() {
     console.log("AAAAAHHHHH");
-    console.log("STOP TOUCHING ME!")
+    console.log("STOP TOUCHING ME!");
 }
 
 btn.onmouseenter = scream;
 
+document.querySelector("h1").onclick = () => {
+    alert("you clicked the h1!");
+};
 
-document.querySelector('h1').onclick = () => {
-    alert('you clicked the h1!')
-}
-
-
-const btn3 = document.querySelector('#v3');
-btn3.addEventListener('click', function () {
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("auxclick", function () {
     alert("CLICKED!");
-})
+});
 
 function twist() {
-    console.log("TWIST!")
+    console.log("TWIST!");
 }
 function shout() {
-    console.log("SHOUT!")
+    console.log("SHOUT!");
 }
 
-const tasButton = document.querySelector('#tas');
+const tasButton = document.querySelector("#tas");
 
+// can't have more THAN ONE function; it overwrittens
 // tasButton.onclick = twist;
 // tasButton.onclick = shout;
 
-tasButton.addEventListener('click', twist)
-tasButton.addEventListener('click', shout)
+// we can have as many event listeners as we need
+// use this method:
+tasButton.addEventListener("click", twist);
+tasButton.addEventListener("click", shout);
