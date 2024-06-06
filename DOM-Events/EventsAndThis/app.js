21bf63834cbea9e706b6bc85dd9bf437f44decb4
Roberto Gutierrez <rob7th@gmail.com>
2024-06-06 17:41:11 -0600

    update

--- DOM-Events/EventsAndThis/app.js
+++ DOM-Events/EventsAndThis/app.js
@@ -3,17 +3,17 @@ const makeRandColor = () => {
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
     return `rgb(${r}, ${g}, ${b})`;
-}
+};
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 
 for (let button of buttons) {
-    button.addEventListener('click', colorize)
+    button.addEventListener("click", colorize);
 }
 
-const h1s = document.querySelectorAll('h1');
+const h1s = document.querySelectorAll("h1");
 for (let h1 of h1s) {
-    h1.addEventListener('click', colorize)
+    h1.addEventListener("click", colorize);
 }
 
 function colorize() {
