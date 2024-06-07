5e339456f43a1ceb1150f2d2ed25b397a21a796a
Roberto Gutierrez <rob7th@gmail.com>
2024-06-07 09:54:26 -0600

    update

--- DOM-Events/exam01/app.js
+++ DOM-Events/exam01/app.js
@@ -1,20 +1,20 @@
 // Leave the next line, the form must be assigned to a variable named 'form' in order for the exercise test to pass
 const form = document.querySelector("#form");
 const formContainer = document.querySelector("#list");
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    const qtyInput = form.elements.qty;
-    const productInput = form.elements.product;
-    addProduct(qtyInput.value, productInput.value);
-    qtyInput.value = "";
-    productInput.value = "";
+    // create list
+    const qtyInput = document.getElementById("qty").value;
+    const productInput = document.getElementById("product").value;
+
+    // create product for list
+    const addProduct = document.createElement("li");
+    addProduct.textContent = `${qtyInput} ${productInput}`;
+
+    // add product to list
+    formContainer.appendChild(addProduct);
+
+    // clean form
+    form.reset();
 });
-
-const addProduct = (qty, product) => {
-    const newProduct = document.createElement("li");
-    const productName = document.createElement("b");
-    productName.append(product);
-    newProduct.append(`${qty} ${product}`);
-    formContainer.append(newProduct);
-};
