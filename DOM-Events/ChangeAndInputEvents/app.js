68730fa8b8f348692c4b3a9fd6aa98dd75bf172b
Roberto Gutierrez <rob7th@gmail.com>
2024-06-07 13:45:02 -0600

    update

--- DOM-Events/ChangeAndInputEvents/app.js
+++ DOM-Events/ChangeAndInputEvents/app.js
@@ -1,10 +1,16 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
 
 // input.addEventListener('change', function (e) {
 //     console.log("CASKDJASKJHD")
 // })
 
-input.addEventListener('input', function (e) {
+input.addEventListener("input", function (e) {
     h1.innerText = input.value;
-})
+    if (input.value) {
+        document.getElementById("bodyOfDoc").style.backgroundColor = "lightcyan";
+    } else {
+        document.getElementById("bodyOfDoc").style.backgroundColor = "bisque";
+        h1.innerText = "done?";
+    }
+});
---
+++ DOM-Events/ChangeAndInputEvents/change-h1.js
@@ -0,0 +1,15 @@
+// variables
+const h1 = document.querySelector("h1");
+const input = document.querySelector("input");
+
+// functions
+input.addEventListener("input", function (event) {
+    h1.innerText = input.value;
+    if (input.value) {
+        document.querySelector("h1").innerText = `Welcome, ${input.value}`;
+        document.querySelector("h1").style.backgroundColor = "lightgray";
+    } else {
+        document.querySelector("h1").innerText = "Enter Your Username";
+        document.querySelector("h1").style.backgroundColor = "white";
+    }
+});
