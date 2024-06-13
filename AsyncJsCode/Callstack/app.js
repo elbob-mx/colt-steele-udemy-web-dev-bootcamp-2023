a3b98ad5053f230d9965dbac089b2532f760891a
Roberto Gutierrez <rob7th@gmail.com>
2024-06-13 00:09:25 -0600

    update

--- AsyncJsCode/Callstack/app.js
+++ AsyncJsCode/Callstack/app.js
@@ -1,11 +1,10 @@
 const multiply = (x, y) => x * y;
 
-const square = x => multiply(x, x);
+const square = (x) => multiply(x, x);
 
-const isRightTriangle = (a, b, c) => (
-    square(a) + square(b) === square(c)
-)
-console.log("BEFORE")
-isRightTriangle(3, 4, 5)
+const isRightTriangle = (a, b, c) => square(a) + square(b) === square(c);
+console.log("true");
 
-console.log("DONEEEE!")
+isRightTriangle(3, 4, 5);
+
+console.log("DONEEEE!");
---
+++ AsyncJsCode/Callstack/loading.js
@@ -0,0 +1,49 @@
+console.log("iniciando proceso:");
+setTimeout(() => {
+    console.log(".");
+}, 2000);
+setTimeout(() => {
+    console.log("..");
+}, 4000);
+setTimeout(() => {
+    console.log("...");
+}, 6000);
+setTimeout(() => {
+    console.log("....");
+}, 8000);
+setTimeout(() => {
+    console.log(".....");
+}, 10000);
+setTimeout(() => {
+    console.log("acelerando RAM");
+}, 10500);
+setTimeout(() => {
+    console.log("......");
+}, 11500);
+setTimeout(() => {
+    console.log(".......");
+}, 12000);
+setTimeout(() => {
+    console.log("........");
+}, 12500);
+setTimeout(() => {
+    console.log(".........");
+}, 13000);
+setTimeout(() => {
+    console.log("..........");
+}, 13500);
+setTimeout(() => {
+    console.log("cambiando iconos!");
+}, 14500);
+setTimeout(() => {
+    console.log("♠♠♠♠♠♠♠♠♠♠♠♠♠");
+}, 15000);
+setTimeout(() => {
+    console.log("♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠");
+}, 15500);
+setTimeout(() => {
+    console.log("♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠");
+}, 16000);
+setTimeout(() => {
+    console.log("♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠♠");
+}, 16500);
