a4a0026d284345288179b1d38784acf1532f0a57
Roberto Gutierrez <rob7th@gmail.com>
2024-08-06 10:49:35 -0600

    update asyncjscode app

--- AsyncJsCode/AsyncFunctions/app.js
+++ AsyncJsCode/AsyncFunctions/app.js
@@ -71,7 +71,7 @@ const fakeRequest = (url) => {
     return new Promise((resolve, reject) => {
         const delay = Math.floor(Math.random() * 4500) + 500;
         setTimeout(() => {
-            if (delay > 2000) {
+            if (delay > 4000) {
                 reject("Connection Timeout :(");
             } else {
                 resolve(`Here is your fake data from ${url}`);
