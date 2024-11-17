960f69ec05904522b470bc9e4d0b0da9d6824f90
Roberto Gutierrez <rob7th@gmail.com>
2024-11-17 15:07:32 -0600

    tv show seaarch app thing

---
+++ axios/tvShowSearch/app.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("#searchForm");
+form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const userQuery = form.elements.query.value;
+    const config = { params: { q: userQuery } };
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    createImage(res.data);
+    form.elements.query.value = "";
+});
+
+const createImage = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            console.log(result);
+            const img = document.createElement("IMG");
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+};
