24c1a14924fe06179005773f3e4748c20e93a2b6
Roberto Gutierrez <rob7th@gmail.com>
2024-11-16 23:13:08 -0600

    update dadJokes app

--- axios/app.js
+++ axios/app.js
@@ -7,15 +7,37 @@
 //         console.log("An error occured: ", e);
 //     });
 
-const getStarWarsCharacterInfo = async (id) => {
+// const getStarWarsCharacterInfo = async (id) => {
+//     try {
+//         const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+//         console.log(res.data);
+//     } catch (e) {
+//         console.log("An error occured: ", e);
+//     }
+// };
+
+// getStarWarsCharacterInfo(1);
+// getStarWarsCharacterInfo(4);
+// getStarWarsCharacterInfo(84); // last available is 83 -> error!
+
+const jokes = document.querySelector("#jokes");
+const jokeBtn = document.querySelector("#jokeBtn");
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    console.log(jokeText);
+    const newLi = document.createElement("Li");
+    newLi.append(jokeText);
+    jokes.append(newLi);
+};
+const getDadJoke = async () => {
     try {
-        const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
-        console.log(res.data);
+        const config = { headers: { Accept: "application/json" } };
+        const res = await axios.get("https://icanhazdadjoke.com/", config);
+        return res.data.joke;
     } catch (e) {
-        console.log("An error occured: ", e);
+        return "no jokes available. Call your dad over the phone or something.";
     }
 };
 
-getStarWarsCharacterInfo(1);
-getStarWarsCharacterInfo(4);
-getStarWarsCharacterInfo(100); // error!
+jokeBtn.addEventListener("click", addNewJoke);
