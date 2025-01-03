e1c5415fd88adcd5e01bb74aacaa7e66e8922d99
Roberto Gutierrez <rob7th@gmail.com>
2025-01-02 21:40:51 -0600

    update Extends Super JS

--- OOP-and-Prototypes/07_Extends_and_Super/app.js
+++ OOP-and-Prototypes/07_Extends_and_Super/app.js
@@ -1,30 +1,36 @@
 class Pet {
-	constructor(name, age) {
-		console.log('IN PET CONSTRUCTOR!');
-		this.name = name;
-		this.age = age;
-	}
-	eat() {
-		return `${this.name} is eating!`;
-	}
+    constructor(name, age) {
+        console.log("IN PET CONSTRUCTOR!");
+        this.name = name;
+        this.age = age;
+    }
+    eat() {
+        return `${this.name} is eating!`;
+    }
 }
 
 class Cat extends Pet {
-	constructor(name, age, livesLeft = 9) {
-		console.log('IN CAT CONSTRUCTOR!');
-		super(name, age);
-		this.livesLeft = livesLeft;
-	}
-	meow() {
-		return 'MEOWWWW!!';
-	}
+    constructor(name, age, livesLeft = 9) {
+        console.log("IN CAT CONSTRUCTOR!");
+        super(name, age);
+        this.livesLeft = livesLeft;
+    }
+    meow() {
+        return "MEOWWWW!!";
+    }
+    eat() {
+        return `${this.name} ignores her food!`;
+    }
 }
 
 class Dog extends Pet {
-	bark() {
-		return 'WOOOF!!';
-	}
-	eat() {
-		return `${this.name} scarfs his food!`;
-	}
+    bark() {
+        return "WOOOF!!";
+    }
+    eat() {
+        return `${this.name} elegantly eats her meaty treats!`;
+    }
 }
+
+const leopolda = new Cat("Leopolda", 8);
+console.log("Lady " + leopolda.eat()); // "Leopolda scarfs his food!" // "Leopolda scarfs his food!"
