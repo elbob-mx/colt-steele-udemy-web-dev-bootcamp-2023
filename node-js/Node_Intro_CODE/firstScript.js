3b4159d82eed32e3de4c342e4d7b69ec8d411884
Roberto Gutierrez <rob7th@gmail.com>
2025-01-22 20:15:19 -0600

    update

--- node-js/Node_Intro_CODE/firstScript.js
+++ node-js/Node_Intro_CODE/firstScript.js
@@ -1,3 +1,7 @@
 for (let i = 0; i < 10; i++) {
-    console.log("HELLO FROM FIRST SCRIPT!!")
+    console.log("HELLO FROM FIRST SCRIPT!!");
 }
+
+// Array.from({ length: 10 }).forEach(() => {
+//     console.log("HELLO FROM FIRST SCRIPT!!");
+// });
