58d5e455d38f3c939c866560789c51b55fb58c8b
Roberto Gutierrez <rob7th@gmail.com>
2025-01-26 17:54:02 -0600

    update cowsay

--- Modules_And_NPM_CODE/Jokester/index.js
+++ Modules_And_NPM_CODE/Jokester/index.js
@@ -1,6 +1,7 @@
 const jokes = require("give-me-a-joke");
+console.log(jokes);
 const colors = require("colors");
 
 jokes.getRandomDadJoke(function (joke) {
-    console.log(joke.rainbow);
+    console.log(joke.underline.green);
 });
--- Modules_And_NPM_CODE/shelter/index.js
+++ Modules_And_NPM_CODE/shelter/index.js
@@ -1,7 +1,9 @@
-const blue = require('./blue')
-const sadie = require('./sadie')
-const janet = require('./janet')
+const blue = require("./blue");
+const sadie = require("./sadie");
+const janet = require("./janet");
 
-const allCats = [blue, sadie, janet]
+const allCats = [blue, sadie, janet];
 
 module.exports = allCats;
+
+console.log(allCats); // [ { name: 'blue', color: 'grey' }, { name: 'sadie', color: 'black' }, { name: 'janet', color: 'orange' } ]
