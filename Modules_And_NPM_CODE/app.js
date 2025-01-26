414ee8e6b7810b49a882296a8b8641463522b54b
Roberto Gutierrez <rob7th@gmail.com>
2025-01-26 16:05:01 -0600

    update app / math js

--- Modules_And_NPM_CODE/app.js
+++ Modules_And_NPM_CODE/app.js
@@ -1,7 +1,8 @@
-const { PI, square } = require('./math');
+const { add, PI, square, text } = require("./math");
 
-const cats = require('./shelter')
+const cats = require("./shelter");
 // console.log(PI)
 
 // console.log(square(9))
-console.log("REQUIRED AN ENTIRE DIRECTORY!", cats)
+console.log("REQUIRED AN ENTIRE DIRECTORY!", cats);
+console.log(text);
--- Modules_And_NPM_CODE/math.js
+++ Modules_And_NPM_CODE/math.js
@@ -2,14 +2,15 @@ const add = (x, y) => x + y;
 
 const PI = 3.14159;
 
-const square = x => x * x;
+const square = (x) => x * x;
+
+const text = "this is new text!";
 
 exports.square = square;
 exports.PI = PI;
+exports.text = text;
 
 // =========================
 // module.exports.add = add;
 // module.exports.PI = PI;
 // module.exports.square = square;
-
-
