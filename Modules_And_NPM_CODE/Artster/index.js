16eb8d1167b73001c9a02c5d207043dfafc9ef3a
Roberto Gutierrez <rob7th@gmail.com>
2025-01-26 19:41:47 -0600

    update localhost chat

--- Modules_And_NPM_CODE/Artster/index.js
+++ Modules_And_NPM_CODE/Artster/index.js
@@ -1,12 +1,50 @@
 // REMEMBER TO RUN "npm install" in your terminal (in the same directory as this file)
 const figlet = require("figlet");
 const colors = require("colors");
-figlet("PANTERA", function (err, data) {
-    if (err) {
-        localStorage;
-        console.log("Something went wrong...");
-        console.dir(err);
-        return;
+// figlet("figlet demo", function (err, data) {
+//     if (err) {
+//         localStorage;
+//         console.log("Something went wrong...");
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data.green);
+// });
+
+figlet.text(
+    "TEXTO",
+    {
+        font: "3-D",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 80,
+        whitespaceBreak: true,
+    },
+    function (err, data) {
+        if (err) {
+            console.log("Something went wrong...");
+            console.dir(err);
+            return;
+        }
+        console.log(data.rainbow);
     }
-    console.log(data.green);
-});
+);
+
+figlet.text(
+    "TEXTO",
+    {
+        font: "3-D",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 80,
+        whitespaceBreak: true,
+    },
+    function (err, data) {
+        if (err) {
+            console.log("Something went wrong...");
+            console.dir(err);
+            return;
+        }
+        console.log(data.bgMagenta);
+    }
+);
